e178b557aa2a75542e100bae993e7c8b4b9b9d25
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2018-12-05 00:44:46 +0900

    (wip) user model valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name:"Example User", email: "user@exsample.com")
+  end
+
+  test "should valid" do
+    assert @user.valid?
+  end
 end
