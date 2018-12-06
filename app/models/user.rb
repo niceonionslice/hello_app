a3a8c35d46c01314b52988b961acae44f3b4e470
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2018-12-06 23:42:46 +0900

    add validation name, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+              format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+              uniqueness: { case_sensitive: false } # 大文字と小文字を区別しない
 end
