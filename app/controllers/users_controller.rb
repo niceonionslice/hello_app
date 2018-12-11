cc806dd93a8293e2858e699bb5e49dc81cd70ca5
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2018-12-11 23:57:56 +0900

    add User SignUp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    # このparams[:id]はroutesのurlのuser/:idから取得している。
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def create
+    # createは、ユーザー登録時に呼ばれる
+    @user = User.new(user_params) # 実装は終わっていないことに注意！
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  # オプション引数を利用すると引数をハッシュで定義する必要がある。
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
