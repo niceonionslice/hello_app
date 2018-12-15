42cdd80765ba2c6814578542cd51c04ca1d909e6
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2018-12-16 00:34:13 +0900

    ヘッダーにログインリンクを追加。 テストにフラッシュの挙動について説明を追加 ユーザーコントローラーにindexメソッドを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,8 @@ class SessionsController < ApplicationController
       # このフラッシュメッセージは一度表示されると消えずに残ってしまう。
       # リダイレクトを利用した時とはことなり、表示したテンプレートをrenderメソッドで強制的に再レンダリングしても
       # リクエストと見なされないため（なぜ、見なされないのか？）、メッセージは消えない。
+      # flash.nowはそのリクエスト内でしか情報を保持しない（その場でエラーを返して終わりたい場合はこちら）
+      # flashは次のリクエストまで情報を保持する。（次の画面に情報を保持したい場合に有効）
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
