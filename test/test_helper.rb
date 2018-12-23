f98f5bbf7bb2e4d5da4c93cb143e3f156521eee9
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2018-12-23 19:14:55 +0900

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,24 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+# 【重要】統合テストでも同様のヘルパーを実装していきます。
+# ただし統合テストではsessionを直接取り扱うことができないので、
+# 代わりにSessionsリソースに対してpostを送信することで代用します (リスト 8.23)。
+# メソッド名は単体テストと同じ、log_in_asメソッドとします。
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session:
+                                { email: user.email,
+                                  password: password,
+                                  remember_me: remember_me
+                                }
+                              }
+  end
 end
