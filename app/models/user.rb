d0b6f1c714c8f3548fbeab8dce0860bb7bf42c4c
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2018-12-23 13:43:03 +0900

    fogetメソッドのコメントを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  # remember_digestをnullにすることで永続セッションを開放する。
   def forget
     update_attribute(:remember_digest, nil)
   end
