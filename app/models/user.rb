9480da361d819feeaff380ec8838e644a8bbc036
kojiro.yamashiro <kojiro.yamashiro@glv.co.jp>
2019-02-03 22:11:35 +0900

    completed password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,8 +54,10 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    # update_attribute(:reset_digest, User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
+    # 上記のリファクタリング
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   def send_password_rest_email
--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -14,5 +14,114 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     # 頑張れ！頑張れ！
     # https://railstutorial.jp/chapters/password_reset?version=5.1#sec-password_reset_test
     # 12.18から
+
+    get new_password_reset_url
+    assert_template 'password_resets/new'
+
+    # メールアドレス無効
+    post password_resets_path, params:{password_reset:{email: ""}}
+    assert_not flash.empty?
+    assert_template 'password_resets/new'
+
+    # メールアドレス有効
+    post password_resets_path, params:{password_reset:{email: @user.email}}
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_not flash.empty?
+    assert_redirected_to root_url
+
+    # パスワード再設定フォームのテスト
+    user = assigns(:user)
+
+    # メールアドレスが無効
+    get edit_password_reset_path(user.reset_token, email:"")
+    assert_not flash.empty?
+    assert_redirected_to root_url
+
+    # 無効なユーザー
+    user.toggle!(:activated)
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_not flash.empty?
+    assert_redirected_to root_url
+    user.toggle!(:activated)
+
+    # メールアドレスが有効で、トークンが無効
+    get edit_password_reset_path('wrong token', email: user.email)
+    assert_not flash.empty?
+    assert_redirected_to root_url
+
+    # メールアドレスもトークンも有効
+     get edit_password_reset_path(user.reset_token, email: user.email)
+     assert_template 'password_resets/edit'
+     # inputタグがあるのかどうかを確認する下記のコードはこのようなコードがあることを確認している。
+     # <input id='email' name='email' type='hidden' value="{{user.email}}"
+     assert_select "input[name=email][type=hidden][value=?]", user.email
+
+     # 無効なパスワードとパスワード確認
+     patch password_reset_path(user.reset_token),
+      params:{ email: user.email,
+                user: {password: "foobar",
+                       password_confirmation: "barquux"}}
+     assert_select 'div#error_explanation'
+
+     # パスワードが空
+     patch password_reset_path(user.reset_token),
+     params:{ email: user.email,
+               user: {password: "",
+                      password_confirmation: ""}}
+     assert_select 'div#error_explanation'
+
+     # 有効なパスワードとパスワード確認
+     patch password_reset_path(user.reset_token),
+      params: {email: user.email,
+              user: { password: "foobar",
+                      password_confirmation: "foobar" }}
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to user
   end
+
+
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path, params: {password_reset: { email: @user.email }}
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+
+    patch password_reset_path(@user.reset_token),
+      params:{ email: @user.email,
+        user: { password: "foobar",
+                password_confirmation: "foobar" }}
+    assert_response :redirect
+    follow_redirect!
+    # 正規表現を利用して本文に特定の文字列が含まれていないかを確認
+    assert_match /expired/i, response.body
+  end
+
+  # パスワードリセットするための画面ではhiddonでメールアドレスを持っているがinputの定義では[name=email]と定義しているのでパラメータで取得するときには
+  # params { email: hogehoge} という感じで取得する。
+  # パスワードは新しいパスワードと再入力用のパスワードが存在しておりまたユーザーに紐付いていることから
+  # params { user: { password: hogehoge, password_confirmation: hogehoge}} と定義する必要がある。
+
+  # <form
+  #   class="edit_user"
+  #   id="edit_user_1"
+  #   action="/password_resets/1foNqAT7v_0C--x4_DtoNQ"
+  #   accept-charset="UTF-8"
+  #   method="post">
+  #
+  #   <input name="utf8" type="hidden" value="✓">
+  #   <input type="hidden" name="_method" value="patch">
+  #   <input type="hidden" name="authenticity_token" value="vBWXuzIiZ7BA3Ob4LEJiVREuNdakkhiPESR+FLOm3xYBjChLsqjzt8k1RqJDKto1+7iT7Yk9YRPRP4lFX/i3OQ==">
+  #   <input type="hidden" name="email" id="email" value="ichi_taro@example.org">
+  #
+  #   <label for="user_password">Password</label>
+  #   <input class="form-control" type="password" name="user[password]" id="user_password" aria-autocomplete="list">
+  #
+  #   <label for="user_password_confirmation">Confirmation</label>
+  #   <input class="form-control" type="password" name="user[password_confirmation]" id="user_password_confirmation">
+  #
+  #   <input type="submit" name="commit" value="Update password" class="btn btn-primary" data-disable-with="Update password">
+  # </form>
 end
